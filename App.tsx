dd30190fc21faa586315846d61147db0f44aee24
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-06-13 16:34:14 -0300

    Separando button

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import React, { useImperativeHandle, useCallback, forwardRef, useState, useRef, 
 import { StyleSheet, Pressable, Image, Text, View, Modal, FlatList, Platform, ImageSourcePropType } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { MaterialIcons } from "@expo/vector-icons";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { GestureHandlerRootView, GestureDetector, Gesture } from "react-native-gesture-handler";
 import Animated, { useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated";
+
+import Button from "./assets/components/Button";
 
 interface EmojiStickerRef {
    setStickerSource: React.Dispatch<React.SetStateAction<ImageSourcePropType | null>>;
@@ -124,91 +125,6 @@ const ImageViewer: React.ForwardRefExoticComponent<React.RefAttributes<ImageView
          </View>
       );
    }
-);
-
-const buttonStyles = StyleSheet.create({
-   container: {
-      marginHorizontal: 20,
-      justifyContent: "center",
-      alignItems: "center",
-      padding: 3,
-      height: 68,
-      width: 320,
-   },
-   label: {
-      fontSize: 16,
-      color: "#fff",
-   },
-   button: {
-      justifyContent: "center",
-      flexDirection: "row",
-      borderRadius: 10,
-      alignItems: "center",
-      height: "100%",
-      width: "100%",
-      gap: 8,
-   },
-   circleButtonContainer: {
-      width: 84,
-      height: 84,
-      marginHorizontal: 60,
-      borderWidth: 4,
-      borderColor: "#ffd33d",
-      borderRadius: 42,
-      padding: 3,
-   },
-   circleButton: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      borderRadius: 42,
-      backgroundColor: "#fff",
-   },
-   iconButton: {
-      justifyContent: "center",
-      alignItems: "center",
-   },
-   iconButtonLabel: {
-      color: "#fff",
-      marginTop: 12,
-   },
-});
-
-interface ButtonProps {
-   onPress?: () => Promise<void>;
-   theme?: "circle-button" | "icon-button" | "primary";
-   label?: string;
-   icon?: keyof typeof MaterialIcons.glyphMap;
-}
-
-const Button: React.FC<ButtonProps> = ({ onPress, theme, label, icon }) => (
-   <>
-      {theme === "primary" ? (
-         <View style={[buttonStyles.container, { borderRadius: 18, borderWidth: 4, borderColor: "#ffd33d" }]}>
-            <Pressable onPress={onPress} style={[buttonStyles.button, { backgroundColor: "#fff" }]}>
-               <FontAwesome color="#25292e" name="picture-o" size={18} />
-               <Text style={[buttonStyles.label, { color: "#25292e" }]}>{label}</Text>
-            </Pressable>
-         </View>
-      ) : theme === "circle-button" ? (
-         <View style={buttonStyles.circleButtonContainer}>
-            <Pressable style={buttonStyles.circleButton} onPress={onPress}>
-               <MaterialIcons name="add" size={38} color="#25292e" />
-            </Pressable>
-         </View>
-      ) : theme === "icon-button" ? (
-         <Pressable style={buttonStyles.iconButton} onPress={onPress}>
-            <MaterialIcons name={icon} size={24} color="#fff" />
-            <Text style={buttonStyles.iconButtonLabel}>{label}</Text>
-         </Pressable>
-      ) : (
-         <View style={buttonStyles.container}>
-            <Pressable onPress={onPress} style={buttonStyles.button}>
-               <Text style={buttonStyles.label}>{label}</Text>
-            </Pressable>
-         </View>
-      )}
-   </>
 );
 
 const modalEmojiPickerStyles = StyleSheet.create({
